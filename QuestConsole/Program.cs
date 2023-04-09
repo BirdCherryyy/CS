c6773d16ab56a47fca99a0b8646776a862098b8a
Oleg <kolesnikovovov@gmail.com>
2023-04-09 17:02:24 +0500

    First commit

---
+++ QuestConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+// See https://aka.ms/new-console-template for more information
+namespace Questions.QuesrConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world!");
+        }
+    }
+}
